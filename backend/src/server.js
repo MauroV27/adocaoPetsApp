b8496617411be9d33a85856db9068c9a2aa35861
daspeon <175569928+daspeon@users.noreply.github.com>
2024-10-28 10:56:16 -0300

    fix: import cors dependency

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
+
+import routes from "./routes/routes.js";
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 app.get("/", (_, response) => {
   response.send({
