53a9b0ecd7279b8c8456e46055af10394d496148
daspeon <175569928+daspeon@users.noreply.github.com>
2024-10-28 10:29:43 -0300

    feat: create home route and set up cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_, response) => {
+  response.send({
+    message:
+      "Welcome to AdoçãoTech API. To see full documentation, please go to /documentation.",
+  });
+});
 
 app.listen(PORT, () => {
-    console.log("Server running on port : ", PORT)
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
