a43deb70d9cc4394b035c039be073dc4b2435f46
MauroV27 <maurovictor13@gmail.com>
2024-10-31 23:44:21 -0300

    feat: Added user methods and routes for, create, login, getById and promote user to admin

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { petsRoutes } from "./petsRoutes.js";
+import { usersRoutes } from "./userRoutes.js";
 
 const routes = Router();
 
 routes.use("/pets", petsRoutes);
+routes.use("/", usersRoutes);
 
 export default routes;
