5b92b82547d06f92687873d8cee0eb98e282f5a3
FranciscoTei <francisco2mix@gmail.com>
2024-11-05 15:35:18 -0300

    refactor: remove Swagger imports from server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,15 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/routes.js";
-import swaggerUi from "swagger-ui-express";
-
-import swaggerFile from "./swagger-output.json" assert { type: "json" };
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`API documentation: http://localhost:${PORT}/docs`);
+  console.log(`API documentation: http://localhost:${PORT}`);
 });
