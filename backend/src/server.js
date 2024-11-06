d9dd647175060695fb1f81247cb17b9ad6e1394c
FranciscoTei <francisco2mix@gmail.com>
2024-11-05 22:09:02 -0300

    refactor: refactor Express app initialization in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,7 @@
-import express from "express";
-import cors from "cors";
-import routes from "./routes/routes.js";
-
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", routes);
+import { app } from "./app.js";
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`API documentation: http://localhost:${PORT}`);
+  console.log(`API documentation: http://localhost:${PORT}/docs`);
 });
