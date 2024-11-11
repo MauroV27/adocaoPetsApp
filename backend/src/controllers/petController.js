f903c098a0f0003dfbe063962a9c291dc8a85679
FranciscoTei <francisco2mix@gmail.com>
2024-11-11 01:46:53 -0300

    feat: implement pet routes handlers

--- backend/src/controllers/petController.js
+++ backend/src/controllers/petController.js
@@ -1,71 +1,99 @@
-import { prismaClient } from "../database/prismaClient.js";
-export async function create(req, res) {
-  /**
-      #swagger.tags = ['Pets']
-      #swagger.description = 'Endpoint to create a new pet.'
-      #swagger.summary = 'Create a new pet.'
-  */
+import { PrismaClient } from "@prisma/client";
+const prismaClient = new PrismaClient();
 
-  try {
-    const { name, specie, breed, gender, dob, description, size, personality } =
-      req.body;
+export function create(req, res) {
+  const { name, specie, dob, description, breed, gender, size, personality } = req.body;
+  const isoDob = new Date(dob).toISOString();
 
-    const pet = await prismaClient.pet.create({
-      data: {
-        name,
-        specie,
-        breed,
-        gender,
-        dob: new Date(dob),
-        description,
-        size,
-        personality,
-      },
-    });
-    // #swagger.responses[201] = { description: 'User registered successfully.' }
-    return res.status(201).json({ pet });
-  } catch (error) {
-    // #swagger.responses[500] = { description: 'Server error.' }
-    return res.status(500).json({ error: error.message });
-  }
+  prismaClient.pet.create({
+    data: {
+      name,
+      specie,
+      dob: isoDob,
+      description,
+      breed,
+      gender,
+      size,
+      personality
+    },
+    select: { id: true, name: true, specie: true, dob: true, description: true, breed: true, gender: true, status: true, size: true, personality: true }
+  })
+  .then(newPet => res.status(201).json({ message: "Pet created successfully", pet: newPet }))
+  .catch(error => res.status(500).json({ error: error.message }));
 }
 
-export async function getAll(req, res) {
-  const { limit = 10, offset = 0 } = req.query;
-  /**
-    #swagger.tags = ['Pets']
-    #swagger.description = 'Endpoint to get all pets.'
-    #swagger.summary = 'Get all pets.'
-  */
-  try {
-    const pets = await prismaClient.pet.findMany({
-      skip: parseInt(offset),
-      take: parseInt(limit),
-      select: {
-        id: true,
-        name: true,
-        specie: true,
-        breed: true,
-        gender: true,
-        dob: true,
-        description: true,
-        size: true,
-        personality: true,
-      },
-    });
-    // #swagger.responses[200] = { description: 'Successful request.' }
-    return res.status(200).json({ pets });
-  } catch (error) {
-    console.error({ error });
-    // #swagger.responses[500] = { description: 'Server error.' }
-    return res.status(500).json({ error: error.message });
-  }
+export function getPetById(req, res) {
+  const { id } = req.params;
+
+  prismaClient.pet.findUnique({
+    where: { id },
+    select: { id: true, name: true, specie: true, dob: true, description: true, breed: true, gender: true, status: true, size: true, personality: true }
+  })
+  .then(pet => {
+    if (pet) {
+      res.status(200).json(pet);
+    } else {
+      res.status(404).json({ message: "Pet not found" });
+    }
+  })
+  .catch(error => res.status(500).json({ error: error.message }));
 }
 
-export class PetController {
-  async getOneById(req, res) {}
+export function getAll(req, res) {
+  const { limit = 10, offset = 0, size, personality, gender } = req.query;
 
-  async update(req, res) {}
+  const filters = {};
+  if (size) filters.size = size;
+  if (personality) filters.personality = personality;
+  if (gender) filters.gender = gender;
 
-  async delete(req, res) {}
+  prismaClient.pet.findMany({
+    skip: parseInt(offset),
+    take: parseInt(limit),
+    where: filters,
+    select: { id: true, name: true, specie: true, dob: true, description: true, breed: true, gender: true, status: true, size: true, personality: true }
+  })
+  .then(pets => {
+    if (pets > 0) {
+      console.log("pets", pets);
+      res.status(200).json(pets);
+    } else {
+      res.status(404).json({ message: "No pets found" });
+    }
+  })
+  .catch(error => res.status(500).json({ error: error.message }));
+}
+
+export function update(req, res) {
+  const { id } = req.params;
+  const { name, specie, dob, description, breed, gender, status, size, personality } = req.body;
+  const isoDob = new Date(dob).toISOString();
+
+  prismaClient.pet.update({
+    where: { id },
+    data: {
+      name,
+      specie,
+      dob: isoDob,
+      description,
+      breed,
+      gender,
+      status,
+      size,
+      personality
+    },
+    select: { id: true, name: true, specie: true, dob: true, description: true, breed: true, gender: true, status: true, size: true, personality: true }
+  })
+  .then(updatedPet => res.status(200).json({ message: "Pet updated successfully", pet: updatedPet }))
+  .catch(error => res.status(500).json({ error: error.message }));
+}
+
+export function deletePet(req, res) {
+  const { id } = req.params;
+
+  prismaClient.pet.delete({
+    where: { id }
+  })
+  .then(() => res.status(204).json({ message: "Pet deleted successfully" }))
+  .catch(error => res.status(500).json({ error: error.message }));
 }
