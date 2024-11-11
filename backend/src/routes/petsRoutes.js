73df9109b957525dc05a838fbc617ced40bb4de1
FranciscoTei <francisco2mix@gmail.com>
2024-11-11 01:47:13 -0300

    chore: integrate pet controllers into routes

--- backend/src/routes/petsRoutes.js
+++ backend/src/routes/petsRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import * as PetController from "../controllers/petController.js";
+import { create, getPetById, getAll, update, deletePet } from '../controllers/petController.js';
+import { authAdminMiddleware } from '../security/jwt-middleware.js';
 
 const routes = Router();
 
-routes.get("/", PetController.getAll);
-routes.post("/", PetController.create);
+routes.post("/pets", create);
+routes.get("/pets/:id", getPetById);
+routes.get("/pets", getAll);
+routes.put("/pets/:id", authAdminMiddleware, update);
+routes.delete("/pets/:id", authAdminMiddleware, deletePet);
+
 
 export { routes as petsRoutes };
