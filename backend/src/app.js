e1ad0dad26cbe2a69a6e7c22ccfcce27ecddb7f1
FranciscoTei <francisco2mix@gmail.com>
2024-11-11 01:46:31 -0300

    feat: configure CORS options for enhanced security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/routes.js";
+import cookieParser from "cookie-parser";
 
+// TODO adicionar corsOptions
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/", routes);
 
 export { app };
