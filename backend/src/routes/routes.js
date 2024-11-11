e6d30b48f851334fb2c30d02c214ea9b7436ffbd
FranciscoTei <francisco2mix@gmail.com>
2024-11-11 01:47:22 -0300

    feat: add Swagger tags to global routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,10 +2,19 @@ import { Router } from "express";
 
 import { petsRoutes } from "./petsRoutes.js";
 import { usersRoutes } from "./userRoutes.js";
+import { adoptionsRoutes } from "./adoptionRoutes.js";
 
 const routes = Router();
+// TODO testar com rotas aqui
+routes.use("/", petsRoutes
+    // #swagger.tags = ['Pets']
+);
 
-routes.use("/pets", petsRoutes);
-routes.use("/", usersRoutes);
+routes.use("/", usersRoutes
+    // #swagger.tags = ['Users']
+);
+routes.use("/", adoptionsRoutes
+    // #swagger.tags = ['Adopt']
+);
 
 export default routes;
