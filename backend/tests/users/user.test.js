5889e3605e977f270e6a0b6cfcbc1d981d2e47a0
MauroV27 <maurovictor13@gmail.com>
2024-11-13 22:07:02 -0300

    test: Added new tests for API user routes

--- backend/tests/users/user.test.js
+++ backend/tests/users/user.test.js
@@ -1,28 +1,8 @@
 import request from "supertest";
 import { app } from "../../src/app";
-
-import { prismaClient } from "../../src/database/prismaClient";
-
-import pkg from 'bcryptjs';
-const { hash } = pkg;
-
-async function utilsCreateUserWithPisma(name, email, phone, address, password, role = "USER") {
-    return await prismaClient.user.create({
-        data : {
-            name, email, phone, address, password: await hash(password, 10), role
-        }
-    }).then( user => user);
-}
+import { utilsCreateUserWithPisma } from "./utils";
 
 describe("User endpoints", () => {
-
-    // beforeAll( async () => {
-
-    //     // Insert ADMIN user in DB  :
-    //     utilsCreateUserWithPisma("admin", "adm@mail.com", "000", "Adm Street", "123456", "ADMIN")
-
-    // });
-
 
     test("Create common User in API successfully", async () => {
 
@@ -49,9 +29,13 @@ describe("User endpoints", () => {
         expect(response.body.user.name).toBe(fakeUserData.name);
     });
 
+    test.todo("Create User with weak password");
+
     test("User Login successfully", async () => {
 
+        // Arrange :
         const fakeUser = await utilsCreateUserWithPisma("User1", "user1@mail.com", "111", "Common Street, n 1", "654");
+        
         // Act : 
         const response = await request(app).post("/login").send({
             "email" : fakeUser.email,
@@ -82,8 +66,97 @@ describe("User endpoints", () => {
 
     });
 
-    test.todo("Get data from User in API successfully");
+    test("Get data from User in API successfully", async () => {
 
-    test.todo("Fail to get data from User in API - User unathourized");
+        // Arrange
+        const fakeUser = await utilsCreateUserWithPisma("UserToGetData", "user2@mail.com", "2222", "Common Street, n 2", "654");
+
+        const loginResponse = await request(app).post("/login").send({
+            "email" : fakeUser.email,
+            "password" : "654"
+        });
+
+        const token = loginResponse.body.token;
+        const id = fakeUser.id;
+
+        // Act : 
+        const response = await request(app).get("/user/" + id).set({
+            "authorization": token,
+            "accept" : "/"
+        });
+
+        // Assert : 
+        const user = response.body.data;
+
+        expect(response.status).toBe(201);
+        
+        // user must contains only id and name :
+        expect(Object.keys(user)).toEqual(Object.keys({'id': 0, 'name': 0, 'email':0, 'phone':0, 'address':0, 'role':0, "Adoption":0}));
+
+        expect(user.id).toBe(fakeUser.id);
+        expect(user.name).toBe(fakeUser.name);
+        expect(user.email).toBe(fakeUser.email);
+        expect(user.phone).toBe(fakeUser.phone);
+        expect(user.address).toBe(fakeUser.address);
+        expect(user.role).toBe(fakeUser.role);
+        expect(user.Adoption).toHaveLength(0);
+    });
+
+    test("Fail to get data from User in API - Not Send Token", async () => {
+
+        // Arrange : 
+        const fakeUser = await utilsCreateUserWithPisma("UserWithout", "user1222@mail.com", "15613150", "Common Street, n 2", "654");
+
+        const loginResponse = await request(app).post("/login").send({
+            "email" : fakeUser.email,
+            "password" : "654"
+        });
+
+        const id = fakeUser.id;
+
+        // Act : 
+        const response = await request(app).get("/user/" + id);
+
+        // Assert : 
+        expect(response.status).toBe(401);
+        expect(response.body.message).toBe("Token not provided");
+    });
+
+
+    test("Fail to get data from User in API - User unathourized", async () => {
+
+        // Arrange : 
+        const fakeUser = await utilsCreateUserWithPisma("Mr Hack", "user2@mrshack.com", "31235", "Common Street, n 2", "654");
+        const fakeUserVictim = await utilsCreateUserWithPisma("Victim", "aaa@mail.com", "131454", "Common Street, n 2", "654");
+
+        const loginResponse = await request(app).post("/login").send({
+            "email" : fakeUser.email,
+            "password" : "654"
+        });
+
+        const token = loginResponse.body.token;
+        const id = fakeUserVictim.id;
+
+        // Act : 
+        const response = await request(app).get("/user/" + id).set({
+            "authorization": token,
+            "accept" : "/"
+        });
+
+        // Assert : 
+        expect(response.status).toBe(403);
+        expect(response.body.message).toBe("Unauthorized access");
+
+    });
+
+    test.todo("Update User with valid data");
+
+    test.todo("Update User with invalid data:email already registered by another user");
+
+    test.todo("Update User with invalid data:phone already registered by another user");
+
+    test.todo("Delete User that never adopted");
+
+    test.todo("Delete User that already adopted");
 
 });
