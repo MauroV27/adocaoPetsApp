4047af1a9b1076de3b143e388099ee7c382a96ed
MauroV27 <maurovictor13@gmail.com>
2024-11-29 21:02:43 -0300

    refactor: Update of pet data descriptions, now with treatment

--- frontend/src/components/pet-card/pet-card.jsx
+++ frontend/src/components/pet-card/pet-card.jsx
@@ -1,36 +1,16 @@
 import React from 'react';
-import { IoMdMale, IoMdFemale, IoMdStar } from "react-icons/io";
-import { PiCatFill, PiDogFill, PiBirdFill, PiRabbitFill, PiQuestionFill } from "react-icons/pi";
+import { IoMdMale, IoMdFemale } from "react-icons/io";
 import { Link } from 'react-router-dom';
 import './pet-card.css';
 import { FaQuestion } from 'react-icons/fa';
 
-// import images from assets : 
-import catImage from '../../assets/pets-images/cat.png';
-import dogImage from '../../assets/pets-images/dog.png';
-import birdImage from '../../assets/pets-images/bird.png';
-import rabbitImage from '../../assets/pets-images/rabbit.png';
-import undefiedImage from '../../assets/pets-images/undefined-300.png';
+import { getSpecieData, getGenderDescription } from '../../constant/petClassification';
 
 function PetCardComponent({pet}) {
 
     const { image, name, gender, description, specie, id } = pet;
 
-    const specieIcon = {
-        "CAT" : <PiCatFill />,
-        "DOG" : <PiDogFill />,
-        "BIRD" : <PiBirdFill />,
-        "RABBIT" : <PiRabbitFill />,
-        "UNDEFINED" : <IoMdStar />
-    }
-
-    const specieIconDescription = {
-        "CAT" : "Gato",
-        "DOG" : "Cachorro",
-        "BIRD" : "Pássaro",
-        "RABBIT" : "Coelho",
-        "UNDEFINED" : `Animal da espécie : ${(specie + '').toLowerCase()}`
-    }
+    const petDataDescription = getSpecieData(specie);
 
     const genderIcon = {
         "FEMALE" : <IoMdFemale />,
@@ -38,26 +18,11 @@ function PetCardComponent({pet}) {
         "UNDEFINED" : <FaQuestion />,
     }
 
-    const genderIconDescription = {
-        "FEMALE" : "Fêmea",
-        "MALE" : "Macho",
-        "UNDEFINED" : "Sexo do pet é desconhecido"
-    }
-
-    // Placeholder images to represent pet specie
-    const placeHolderImage = {
-        "GATO" : catImage,
-        "DOG" : dogImage,
-        "BIRD" : birdImage,
-        "RABBIT" : rabbitImage,
-        "UNDEFINED" : undefiedImage,
-    }
-
     return (
         <Link to={'/pet/' + id} className="card__link">
             <div className="card">
                 <img 
-                    src={specie in placeHolderImage ? (placeHolderImage[specie]) : (placeHolderImage["UNDEFINED"])} 
+                    src={petDataDescription.image} 
                     alt={name} 
                     className="card__image" 
                 />
@@ -67,13 +32,13 @@ function PetCardComponent({pet}) {
 
                         <div className="pet__data__description">
                             <span className="pet__icon__style"
-                                title={specie in specieIconDescription ? specieIconDescription[specie]: specieIconDescription["UNDEFINED"]}
+                                title={petDataDescription.description}
                                 >
-                                {specie in specieIconDescription ? specieIcon[specie]: specieIcon["UNDEFINED"]}
+                                {petDataDescription.icon}
                             </span>
 
                             <span className="pet__icon__style"
-                                title={gender ? genderIconDescription[gender]: genderIconDescription["UNDEFINED"]}
+                                title={getGenderDescription(gender)}
                                 >
                                 {gender ? genderIcon[gender]: genderIcon["UNDEFINED"]}
                             </span>
